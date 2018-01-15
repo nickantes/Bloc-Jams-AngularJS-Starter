d53c200211b4ef5a3e93957344e6bfe80576cd9d
Nick <nick.c.antes@gmail.com>
2018-01-15 09:49:45 -0800

    Song player service update 1.0

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -27,18 +27,39 @@
 
         	currentSong = song;
         };
+        /**
+        * @function playSong
+        * @desc Starts playing the currently selected song and sets the song.playing boolean value
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+        	currentBuzzObject.play();
+        	song.playing = true;
+        };
 
+        /**
+		* @method SongPlayer.play
+		* @desc Checks to see if the song clicked was already the selected song and, if not,
+		* sets the clicked song as the current song & plays it or, if paused,
+		* resumes playing.
+		* @param {Object} song
+        */
         SongPlayer.play = function(song) {
         	if (currentSong !== song) {
         		setSong(song);
-        		currentBuzzObject.play();
-        		song.playing = true;
+        		playSong(song);
         	} else if (currentSong === song) {
         		if (currentBuzzObject.isPaused()) {
         			currentBuzzObject.play();
         		}
         	}
         };
+
+        /**
+		* @method SongPlayer.pause
+		* @desc Pauses the currently playing song and clears the song.playing boolean value
+		* @param {Object} song
+        */
 
         SongPlayer.pause = function(song) {
         	currentBuzzObject.pause();
